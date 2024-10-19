c78e04d999f330d503bd245ca6cbf1564d6915c0
山野優斗 <C2362@oic.jp>
2024-10-20 03:22:59 +0900

    コンテキストに接続文字列の追加

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,9 +5,18 @@ namespace OIC_FK31.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext()
+        {
+        }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database=aspnet-OIC_FK31-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true");
         }
     }
 }
