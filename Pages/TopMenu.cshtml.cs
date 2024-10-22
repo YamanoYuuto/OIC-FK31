dccd08d1b33ba4b5d67766056e33bd400533e649
山野優斗 <C2362@oic.jp>
2024-10-22 17:05:31 +0900

    ログインするとtopmenuniiku

--- Pages/TopMenu.cshtml.cs
+++ Pages/TopMenu.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,25 @@ namespace FK_31.Pages
 {
     public class TopMenuModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<TopMenuModel> _logger;
+
+        public TopMenuModel(SignInManager<IdentityUser> signInManager, ILogger<TopMenuModel> logger, UserManager<IdentityUser> userManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            //<IActionResult>‚É‚µ‚Ä
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+            return Page();
         }
     }
 }
