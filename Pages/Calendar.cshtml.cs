9704ee9d39c54925643ff39600fb1f3eec6d5929
山野優斗 <C2362@oic.jp>
2024-11-05 17:07:57 +0900

    Revert "Info"
    This reverts commit 0bceb4c930a5a6261bed9a9c8d9100c078fa3c67.

--- Pages/Calendar.cshtml.cs
+++ Pages/Calendar.cshtml.cs
@@ -3,7 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace OIC_FK31.Pages
 {
+<<<<<<<< HEAD:Pages/Info.cshtml.cs
+    public class InfoModel : PageModel
+========
     public class CalendarModel : PageModel
+>>>>>>>> origin/3B14SN:Pages/Calendar.cshtml.cs
     {
         public void OnGet()
         {
--- Pages/Info.cshtml.cs
+++ Pages/Info.cshtml.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
 
 namespace OIC_FK31.Pages
 {
@@ -8,50 +7,6 @@ namespace OIC_FK31.Pages
     {
         public void OnGet()
         {
-
-        }
-
-        [BindProperty]
-        public InputModel Input { get; set; }
-
-        
-        public class InputModel
-        {
-          
-            [Required(ErrorMessage = "姓 (Last Name)を入力してください。")]
-            public string last_name { get; set; }
-
-            [Required(ErrorMessage = "名 (First Name)を入力してください。")]
-            public string first_name { get; set; }
-
-            [Required(ErrorMessage = "メールアドレスを入力してください。")]
-            [EmailAddress]
-            public string email { get; set; }
-
-            [Required(ErrorMessage = "メールアドレス（再入力用）を入力してください。")]
-            [EmailAddress]
-            public string email_confirm { get; set; }
-
-            [Required(ErrorMessage = "電話番号を入力してください。")]
-            public string phone { get; set; }
-
-            [Required(ErrorMessage = "電話番号（再入力用）を入力してください。")]
-            public string phone_confirm { get; set; }
-
-            [Required(ErrorMessage = "郵便番号を入力してください。")]
-            public string postal_code { get; set; }
-
-            [Required(ErrorMessage = "都道府県を入力してください。")]
-            public string prefecture { get; set; }
-
-            [Required(ErrorMessage = "市区町村を入力してください。")]
-            public string city { get; set; }
-
-            [Required(ErrorMessage = "丁目・番地を入力してください。")]
-            public string address { get; set; }
-
-            [Required(ErrorMessage = "建物名・部屋番号を入力してください。")]
-            public string building { get; set; }
         }
     }
 }
