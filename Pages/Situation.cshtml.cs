06b8d858d0daed111ec3aea0bf933224cbf0c282
shu_yoshida <C2375@oic.jp>
2024-11-05 15:41:32 +0900

    予約状況確認画面生成

---
+++ Pages/Situation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OIC_FK31.Pages
+{
+    public class SituationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
