c729dc4c54c4bfc12681ee5bfbe920de5b011b4f
山野優斗 <C2362@oic.jp>
2024-11-05 17:09:06 +0900

    04のマージ戻した

--- Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -416,6 +416,7 @@ namespace OIC_FK31.Data.Migrations
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
                 });
+            modelBuilder.Entity<Reserve>();
 #pragma warning restore 612, 618
         }
     }
