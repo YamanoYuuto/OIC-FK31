235d0669d8c8e0ff40dc151e02a30e6751988293
木村望仁 <c2093@oic.jp>
2024-11-08 14:16:28 +0900

    データベース

--- Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -269,7 +269,11 @@ namespace OIC_FK31.Data.Migrations
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
                 });
+            modelBuilder.Entity<facility>();
+            modelBuilder.Entity<reservation>();
+
 #pragma warning restore 612, 618
+
         }
     }
 }
---
+++ Data/Migrations/facility.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OIC_FK31.Data.Migrations
+{
+    public class facility
+    {
+        public int FacilityID { get; set; }
+        public string FacilityName { get; set; }
+        public string FacilityAddress { get; set; }
+        public string FacilityPhone { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime OpeningTime { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime ClosingTime { get; set; }
+        public string FacilityPostCode { get; set; }
+    }
+}
