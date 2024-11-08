f44618db609ea6eb044f83ffae041f0cedd04e80
山野優斗 <C2362@oic.jp>
2024-11-08 14:16:11 +0900

    予約モデル柵状

--- Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -269,7 +269,6 @@ namespace OIC_FK31.Data.Migrations
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
                 });
-            modelBuilder.Entity<Reserve>();
 #pragma warning restore 612, 618
         }
     }
