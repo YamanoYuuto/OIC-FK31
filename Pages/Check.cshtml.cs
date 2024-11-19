607f43eb34d2e31a799337da950199cf9fc88732
山野優斗 <C2362@oic.jp>
2024-11-19 14:48:04 +0900

    予約一連一応完成

--- Pages/Check.cshtml.cs
+++ Pages/Check.cshtml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using OIC_FK31.Data;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FK_31.Pages
 {
@@ -16,76 +18,121 @@ namespace FK_31.Pages
         {
             _userManager = userManager;
         }
-        public InputModel Input { get; set; }
 
-        public class InputModel
-        {
-            public string last_name { get; set; }
+        public string last_name { get; set; }
 
-            public string first_name { get; set; }
+        public string first_name { get; set; }
 
-            public string email { get; set; }
+        public string email { get; set; }
 
-            public string phone { get; set; }
+        public string phone { get; set; }
 
-            public string postal_code { get; set; }
+        public string postal_code { get; set; }
 
-            public string prefecture { get; set; }
+        public string prefecture { get; set; }
 
-            public string city { get; set; }
+        public string city { get; set; }
 
-            public string address { get; set; }
+        public string address { get; set; }
 
-            public string building { get; set; }
+        public string building { get; set; }
 
-            public int facilityid { get; set; }
+        public int facilityid { get; set; }
+        public string facilityname { get; set; }
 
-            [DataType(DataType.DateTime)]
-            public DateTime starttime { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime starttime { get; set; }
 
-            [DataType(DataType.DateTime)]
-            public DateTime endtime { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime endtime { get; set; }
 
 
-        }
-
-        public async Task<IActionResult> OnGetAsync()
+        public async Task<IActionResult> OnGetAsync([FromRoute] string Date)
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
                 return Redirect("/Identity/Account/Login");
             }
+            if (Date != null)
+            {
+                string[] date = Date.Split('!');
+                last_name = date[0];
+                first_name = date[1];
+                //email = date[2];
+                //phone = date[3];
+                //postal_code = date[4];
+                //prefecture = date[5];
+                //city = date[6];
+                //address = date[7];
+                //building = date[8];
+                facilityid = int.Parse(date[9]);
+                starttime = DateTime.Parse(date[10]);
+                endtime = DateTime.Parse(date[11]);
+
+                try
+                {
+                    ApplicationDbContext context = new ApplicationDbContext();
+                    facilityname = context.Facility.Find(int.Parse(date[9])).FacilityName;
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            }
+
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
-        {      
+        public async Task<IActionResult> OnPostAsync([FromRoute] string Date)
+        {
+            if (Date != null)
+            {
+                string[] date = Date.Split('!');
+                last_name = date[0];
+                first_name = date[1];
+                email = date[2];
+                phone = date[3];
+                postal_code = date[4];
+                prefecture = date[5];
+                city = date[6];
+                address = date[7];
+                building = date[8];
+                facilityid = int.Parse(date[9]);
+                starttime = DateTime.Parse(date[10]);
+                endtime = DateTime.Parse(date[11]);
+            }
+            else
+            {
+                return Redirect("/TopMenu");
+            }
+
             var context = new ApplicationDbContext();
 
             var Time = new time
             {
-                FacilityID = Input.facilityid,
-                StartTime = Input.starttime,
-                EndTime = Input.endtime
+                FacilityID = facilityid,
+                StartTime = starttime,
+                EndTime = endtime
             };
 
             context.Time.Add(Time);
             context.SaveChanges();
-            
-                var UserDatail = new userDetail
-                {
-                    UserID = _userManager.GetUserAsync(User).Id,
-                    LastName = Input.last_name,
-                    FirstName = Input.first_name,
-                    Email = Input.email,
-                    Phone = Input.phone,
-                    PostalCode = Input.postal_code,
-                    Prefecture = Input.prefecture,
-                    City = Input.city,
-                    Address = Input.address,
-                    Building = Input.building
-                };
+
+            var UserDatail = new userDetail
+            {
+                UserID = _userManager.GetUserAsync(User).Id,
+                LastName = last_name,
+                FirstName = first_name,
+                Email = email,
+                Phone = phone,
+                PostalCode = postal_code,
+                Prefecture = prefecture,
+                City = city,
+                Address = address,
+                Building = building
+            };
 
             context.UserDetail.Add(UserDatail);
             context.SaveChanges();
@@ -94,12 +141,14 @@ namespace FK_31.Pages
             {
                 UserDetailID = UserDatail.UserDetailID,
                 TimeID = Time.TimeID,
-                BookingDate=DateTime.Now
+                BookingDate = DateTime.Now,
+                Application = "‚È‚µ",
+                Number = 0
             };
             context.Reservation.Add(Reservation);
             context.SaveChanges();
 
-            return Redirect("/Thank");
+            return RedirectToPage("/Thank", new {id = Reservation.ReservationID});
         }
     }
 }
