63eb97bb15fe12241f66af5d67ee7551127825c4
山野優斗 <C2362@oic.jp>
2024-12-03 14:25:34 +0900

    施設管理追加・編集

--- Pages/FacilityAdd.cshtml.cs
+++ Pages/FacilityAdd.cshtml.cs
@@ -1,22 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow.CopyAnalysis;
 using OIC_FK31.Data;
 using System.Security.Claims;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
 
 namespace OIC_FK31.Pages
 {
     public class FacilityAddModel : PageModel
     {
+
+        private readonly IWebHostEnvironment _environment;
         [BindProperty]
         public facility FacilityAdd { get; set; }
 
-        
-        public async Task<IActionResult> OnPostAsync()
+        public FacilityAddModel(IWebHostEnvironment environment)
         {
+            _environment = environment;
+        }
+        public async Task<IActionResult> OnPostAsync([Required(ErrorMessage ="ファイルが選択されていません")]IFormFile photofile)
+        {
+            ModelState.Remove("FacilityAdd.FacilityphotoPath");
+
+            if (photofile != null)
+            {
+                var extension = Path.GetExtension(photofile.FileName).ToLower();
+                if (extension != ".jpg")
+                {
+                    ModelState.AddModelError("FileEx", "ファイルは.jpgのみです。");
+                }
+            }
+            if(FacilityAdd.OpeningTime > FacilityAdd.ClosingTime)
+            {
+                ModelState.AddModelError("TimeError", "閉館時間より閉館時間の方が遅いです。");
+            }
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+
+            if (photofile != null)
+            {
+                FacilityAdd.FacilityphotoPath = photofile.FileName;
+                var uploadsFolder = Path.Combine(_environment.WebRootPath, "images");
+                var filepath = Path.Combine(uploadsFolder, photofile.FileName);
+                var stream = new FileStream(filepath, FileMode.Create);
+                await photofile.CopyToAsync(stream);
+            }
+            
             var context =new ApplicationDbContext();
 
             await context.Facility.AddAsync(FacilityAdd);
--- Pages/FacilityEdit.cshtml.cs
+++ Pages/FacilityEdit.cshtml.cs
@@ -8,8 +8,14 @@ namespace OIC_FK31.Pages
 {
     public class FacilityEditModel : PageModel
     {
+        private readonly IWebHostEnvironment _environment;
         [BindProperty]
         public facility Facility { get; set; }
+
+        public FacilityEditModel(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
@@ -25,18 +31,38 @@ namespace OIC_FK31.Pages
             Facility = facility;
             return Page();
         }
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostAsync(int? id, IFormFile? photofile)
         {
+            ModelState.Remove("Facility.FacilityphotoPath");
+
+            if(photofile != null)
+            {
+                var extension = Path.GetExtension(photofile.FileName).ToLower();
+                if (extension != ".jpg")
+                {
+                    ModelState.AddModelError("FileEx", "ファイルは.jpgのみです。");
+                }
+            }
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
             var context = new ApplicationDbContext();
             var facility = await context.Facility.FindAsync(id);
             if (facility == null)
             {
                 return NotFound();
+            }
+
+            if (photofile != null)
+            {
+                Facility.FacilityphotoPath = photofile.FileName;
+                var uploadsFolder = Path.Combine(_environment.WebRootPath, "images");
+                var filepath = Path.Combine(uploadsFolder, photofile.FileName);
+                var stream = new FileStream(filepath, FileMode.Create);
+                await photofile.CopyToAsync(stream);
+                facility.FacilityphotoPath = photofile.FileName;
             }
 
             facility.FacilityName = Facility.FacilityName;
@@ -45,7 +71,6 @@ namespace OIC_FK31.Pages
             facility.OpeningTime = Facility.OpeningTime;
             facility.ClosingTime = Facility.ClosingTime;
             facility.FacilityPostCode = Facility.FacilityPostCode;
-            facility.FacilityphotoPath = Facility.FacilityphotoPath;
             
 
             try
